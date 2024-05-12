8e7fc3361cca3818e2b75d03c4c968ffc90abcbb
shubhh123 <shubhh1181@gmail.com>
2024-05-12 18:28:40 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_basics_part2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_basics_part2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basics_part2/background.dart';
+
+void handelButtonPress() {}
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(body: StartScreen()),
+  ));
+}
